9afa0b093935d1519106478ba0cac28db6caa386
Abby Berkers <abbyberkers@gmail.com>
2021-01-10 11:54:51 +0100

    Bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 group = "nl.deltadak"
 version = "0.1"
 
 plugins {
 
-    val kotlinVersion = "1.3.72"
+    val kotlinVersion = "1.4.21"
 
     application
     kotlin("jvm") version kotlinVersion
@@ -13,17 +15,18 @@ plugins {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
     // To "prevent strange errors".
     implementation(kotlin("reflect"))
     // Kotlin reflection.
     implementation(kotlin("test"))
     implementation(kotlin("test-junit"))
 
-    implementation("com.pinterest.ktlint:ktlint-core:0.36.0")
-    implementation("com.pinterest.ktlint:ktlint-test:0.36.0")
+    val ktlintVersion = "0.40.0"
+    implementation("com.pinterest.ktlint:ktlint-core:$ktlintVersion")
+    implementation("com.pinterest.ktlint:ktlint-test:$ktlintVersion")
 
-    val kotestVersion = "4.0.5"
+    val kotestVersion = "4.3.2"
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion") // for kotest core jvm assertions
     testImplementation("io.kotest:kotest-property-jvm:$kotestVersion") // for kotest property test
@@ -39,4 +42,10 @@ repositories {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
